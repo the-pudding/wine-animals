9500cdf56795ce2a6578d5247e78ef6f704cdbfc
Jan Diehm <jadiehm@gmail.com>
2024-05-01 16:01:42 -0500

    update github pages

--- docs/_app/immutable/chunks/paths.Cfeeonaf.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1gglrip?.base??"",a=globalThis.__sveltekit_1gglrip?.assets??s;export{a,s as b};
---
+++ docs/_app/immutable/chunks/paths.wyjftiBo.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_y1pntm?.base??"",t=globalThis.__sveltekit_y1pntm?.assets??s;export{t as a,s as b};
