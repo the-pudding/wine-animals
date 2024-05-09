dd45cd10dc1e20b005c7accc661f698e3d31edf7
Jan Diehm <jadiehm@gmail.com>
2024-05-08 21:39:31 -0500

    rerun batch

--- tasks/identify-wine.js
+++ tasks/identify-wine.js
@@ -11,7 +11,7 @@ const testingSet = ["img_2082804.png"];
 const rawRerun = fs.readFileSync("./src/data/rerun.csv", "utf8");
 const csvRerun = d3.csvParse(rawRerun);
 
-const start = 20;
+const start = 983;
 let spent = 0;
 
 const openai = new OpenAI({
