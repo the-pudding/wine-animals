2c1d896fee39f5440b86d5213ab2b045ebfbea18
Jan Diehm <jadiehm@gmail.com>
2024-07-19 15:16:36 -0500

    update github pages

--- docs/_app/immutable/chunks/paths.csxouvXH.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1l974hf?.base??"",a=globalThis.__sveltekit_1l974hf?.assets??s;export{a,s as b};
--- docs/_app/immutable/entry/start.PqH8u8bz.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.XliF_XE3.js";export{t as start};
