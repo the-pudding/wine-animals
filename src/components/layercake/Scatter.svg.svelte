0bfc08b8553bd9d44756babe40b8fd84825cb292
Jan Diehm <jadiehm@gmail.com>
2024-09-24 10:09:57 -0500

    scale on mount

--- src/components/layercake/Scatter.svg.svelte
+++ src/components/layercake/Scatter.svg.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { getContext } from "svelte";
+	import { getContext, onMount } from "svelte";
 	import * as d3 from "d3";
 
 	const { data, xGet, yGet, xScale, yScale } = getContext("LayerCake");
@@ -13,19 +13,19 @@
 
 	const priceAVG = d3.mean($data[0], d => d.price);
 	const ratingAVG = d3.mean($data[0], d => d.rating);
+	let path;
+
+	onMount(async () => {
+       path = d3.line()
+			.x(d => $xScale(d[0]))
+			.y(d => $yScale(d[1]))
+			(regressionLine);
+	});
 	
 
 	console.log($xScale.domain(), $yScale.domain())
 	console.log($xScale.range(), $yScale.range())
-	// console.log($data[1])
 	// console.log($xScale(regressionLine[0][0]), $yScale(regressionLine[0][1]));
-	
-	const path = d3.line()
-		.x(d => $xScale(d[0]))
-		.y(d => $yScale(d[1]))
-		(regressionLine);
-
-		console.log('Generated path:', path);
 </script>
 
 <g>
@@ -39,7 +39,9 @@
 	<line class="priceAVG" x1={0} y1={$yScale(d3.mean($data[0], d => d.price))} x2={250} y2={$yScale(d3.mean($data[0], d => d.price))} />
 	<line class="ratingAVG" x1={$xScale(d3.mean($data[0], d => d.rating))} y1={0} x2={$xScale(d3.mean($data[0], d => d.rating))} y2={220} />
 	<line class="regression" x1={$xScale($data[1][0].x)} y1={$yScale($data[1][0].y)} x2={$xScale($data[1][1].x)} y2={$yScale($data[1][1].y)} />
-	<path class="expRegression" d={path} />
+	{#if path}
+		<path class="expRegression" d={path} />
+	{/if}
 </g>
 
 <style>
