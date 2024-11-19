0fb25ff828f8275b4f2e3e30869cfda7257e0444
Jan Diehm <jadiehm@gmail.com>
2024-11-19 16:31:45 -0600

    update github pages

---
+++ docs/_app/immutable/chunks/paths.AeVUwdK6.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_i7tndr?.base??"",t=globalThis.__sveltekit_i7tndr?.assets??s;export{t as a,s as b};
--- docs/_app/immutable/chunks/paths.MO4fx46o.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_g4520u?.base??"",a=globalThis.__sveltekit_g4520u?.assets??s;export{a,s as b};
